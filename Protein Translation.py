c134b8314673cb3155879fc4ce7a92c5286dc9f8
Rodrigo Pombo <104930834+RodrigoPombo1@users.noreply.github.com>
2022-12-20 14:34:27 +0000

    .

---
+++ Protein Translation.py
@@ -0,0 +1,27 @@
+def proteins(strand):
+    adict = {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine", 
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP",
+    }
+    result = []
+    for i in range(0, len(strand), 3):
+        aux = adict[strand[i:i+3]]
+        if aux == "STOP":
+            break
+        result.append(aux)
+    return result
